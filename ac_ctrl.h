fb1a689c41dcb6ad420bf267d474d7a702eebc27
deviousprophet <69911034+deviousprophet@users.noreply.github.com>
2020-10-20 17:28:11 +0700

    Initial commit

---
+++ ac_ctrl.h
@@ -0,0 +1,48 @@
+#if CONFIG_FREERTOS_UNICORE
+#define ARDUINO_RUNNING_CORE 0
+#else
+#define ARDUINO_RUNNING_CORE 1
+#endif
+
+#include <Arduino.h>
+#include <assert.h>
+#include <IRrecv.h>
+#include <IRsend.h>
+#include <IRremoteESP8266.h>
+#include <IRac.h>
+#include <IRtext.h>
+#include <IRutils.h>
+
+#include <ir_Daikin.h>
+#include <ir_Mitsubishi.h>
+#include <ir_MitsubishiHeavy.h>
+#include <ir_NEC.h>
+#include <ir_RC5_RC6.h>
+#include <ir_Sharp.h>
+
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Hotspot-LATITUDE-E6420";
+const char* password = "31121998";
+const char* mqtt_server = "192.168.137.1";
+const int mqtt_port = 1883;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+const uint32_t kBaudRate = 115200;
+const uint16_t kCaptureBufferSize = 1024;
+const uint16_t kMinUnknownSize = 12;
+
+bool config_ac = false;
+bool ac_configed = false;
+
+#if DECODE_AC
+const uint8_t kTimeout = 50;
+#else
+const uint8_t kTimeout = 15;
+#endif
+
+void TaskIRrecv(void *pvParamaters);
+void TaskIRsend(void *pvParamaters);
---
+++ ac_ctrl.ino
@@ -0,0 +1,90 @@
+#include "ac_ctrl.h"
+
+const uint16_t kRecvPin = 14;
+
+IRrecv irrecv(kRecvPin, kCaptureBufferSize, kTimeout, true);
+decode_results results;
+
+void setup() {
+  Serial.begin(kBaudRate);
+  while (!Serial) delay(50);
+  assert(irutils::lowLevelSanityCheck() == 0);
+  Serial.printf("\n" D_STR_IRRECVDUMP_STARTUP "\n", kRecvPin);
+#if DECODE_HASH
+  irrecv.setUnknownThreshold(kMinUnknownSize);
+#endif
+  irrecv.enableIRIn();
+  
+  delay(10);
+  WiFi.begin(ssid, password);
+  client.setServer(mqtt_server, mqtt_port); 
+  client.setCallback(callback);
+  
+  xTaskCreatePinnedToCore(TaskIRrecv, "IR_recv", 1024, NULL, 3, NULL, ARDUINO_RUNNING_CORE);
+  xTaskCreatePinnedToCore(TaskIRsend, "IR_send", 1024, NULL, 3, NULL, ARDUINO_RUNNING_CORE);
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    WiFi.reconnect();
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected");
+  }
+  
+  if (!client.connected() && (WiFi.status() == WL_CONNECTED)) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP_ac_ctrl")) {
+      Serial.println("connected");
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("");
+      delay(1000);
+    }
+  }
+  client.loop();
+}
+
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+  
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+}
+
+void TaskIRrecv(void *pvParameters) {
+  (void) pvParameters;
+
+  for (;;) {
+    if (config_ac && irrecv.decode(&results)) {
+      if (results.overflow) Serial.printf(D_WARN_BUFFERFULL "\n", kCaptureBufferSize);
+      // Display the basic output of what we found.
+      Serial.print(resultToHumanReadableBasic(&results));
+      // Display any extra A/C info if we have it.
+      String description = IRAcUtils::resultAcToString(&results);
+      if (description.length()) Serial.println(D_STR_MESGDESC ": " + description);
+      yield();
+    }
+  }
+}
+
+void TaskIRsend(void *pvParamaters) {
+  (void) pvParameters;
+
+  for (;;) {
+    if (ac_configed) {
+
+    }
+  }
+}
